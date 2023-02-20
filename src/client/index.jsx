de0f300e68dc16322229709ffe8ab9bc29c541f5
hanadbruno <hanad1997@hotmail.no>
2023-02-20 12:49:39 +0100

    Adding router-links

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "react"
+import {Route, Switch} from "react-router";
+import {BrowserRouter, Link} from "react-router";
 
+
+function Application (){
+    return <BrowserRouter>
+          <Switch>
+            <Route path={"/employees"}>
+                <h1>List all Employees</h1>
+            </Route>
+            <Route path={"/add"}>
+                <h1>Add Employees</h1>
+            </Route>
+            <Route path={"/edit"}>
+                <h1>Remove Employees</h1>
+            </Route>
+            <Route path={"/"}>
+                <h1>Employee Application home page</h1>
+                <ul>
+                    <Link to ={"/Employes"}>See Employees</Link>
+                    <Link to ={"/add"}>Add Employees</Link>
+                    <Link to ={"/edit"}>Remove Employees</Link>
+                </ul>
+            </Route>
+
+          </Switch>
+    </BrowserRouter>
+}
 
 ReactDOM.render(<div>Hello World</div>, document.getElementById("app"));
