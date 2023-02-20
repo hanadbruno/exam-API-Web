d623dc3aac9fca5d064232b4af97d248fbf51fa7
hanadbruno <hanad1997@hotmail.no>
2023-02-20 12:28:00 +0100

    Adding react

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,1 +1,6 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "react"
+
+
+ReactDOM.render(<div>Hello World</div>, document.getElementById("app"));
