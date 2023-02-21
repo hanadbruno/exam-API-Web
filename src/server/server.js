d18168ba3dfa88ce83242cbf5216fa72815bd729
hanadbruno <hanad1997@hotmail.no>
2023-02-21 08:29:10 +0100

    hello world on server

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Started on http://localhost:3000")
+});
+
+app.use((req, res, next) => {
+    res.json({"text": "hello world"})
+})
