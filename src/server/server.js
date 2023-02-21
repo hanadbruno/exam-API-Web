eebc1865173063f5bd23bf5225f5dce39be0607e
hanadbruno <hanad1997@hotmail.no>
2023-02-21 08:43:13 +0100

    express server app

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,17 @@
 
 
 const express = require("express");
+const { dirname } = require("path");
+const path =  require("path")
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
 app.listen(3000, () => {
     console.log("Started on http://localhost:3000")
 });
 
 app.use((req, res, next) => {
-    res.json({"text": "hello world"})
+    res.sendFile(path.resolve(__dirname,"..","..","dist", "index.html"));
 })
